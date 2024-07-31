2f26329a61fa04c524f048b6f19d2543cb5ce518
MembaDavid <davidmemba2@gmail.com>
2024-07-31 06:40:07 +0300

    nan

--- src/app/Agents/page.jsx
+++ src/app/Agents/page.jsx
@@ -3,7 +3,7 @@ import Experience_cards from '@/components/Experience_cards'
 function page() {
   return (
     <div>
-	 Hello there
+	 Hello there David
    <Experience_cards 
     title = "First card"
     description = "This is the first card to be displayed"
